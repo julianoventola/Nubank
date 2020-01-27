5f5f2300d6a07c6eb6b11332ff6aaaacb0850669
JulianoV <ventolajuliano@gmail.com>
2020-01-27 15:39:15 -0300

    Animation for onHandlerStateChanged

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -22,6 +22,8 @@ import {
 } from './styles';
 
 export default function Main() {
+  let offset = 0;
+
   const translateY = new Animated.Value(0);
 
   const animatedEvent = Animated.event(
@@ -35,7 +37,31 @@ export default function Main() {
     {useNativeDriver: true},
   );
 
-  function onHandlerStateChanged(event) {}
+  function onHandlerStateChanged(event) {
+    if (event.nativeEvent.oldState === State.ACTIVE) {
+      let opened = false;
+      const {translationY} = event.nativeEvent;
+      offset += translationY;
+
+      if (translationY >= 100) {
+        opened = true;
+      } else {
+        translateY.setValue(offset);
+        translateY.setOffset(0);
+        offset = 0;
+      }
+
+      Animated.timing(translateY, {
+        toValue: opened ? 380 : 0,
+        duration: 200,
+        useNativeDriver: true,
+      }).start(() => {
+        offset = opened ? 380 : 0;
+        translateY.setOffset(offset);
+        translateY.setValue(0);
+      });
+    }
+  }
 
   return (
     <Container>
